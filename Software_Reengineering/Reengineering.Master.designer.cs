c6dfaa5b17458986da2fa226c5b3405bb8730526
Tee Joohong <teejoohong0823@gmail.com>
2021-09-01 17:52:51 +0800

    jh

--- Software_Reengineering/Reengineering.Master.designer.cs
+++ Software_Reengineering/Reengineering.Master.designer.cs
@@ -132,15 +132,6 @@ namespace Software_Reengineering
         protected global::System.Web.UI.WebControls.HyperLink HyperLink4;
 
         /// <summary>
-        /// HyperLink5 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink5;
-
-        /// <summary>
         /// HyperLink6 control.
         /// </summary>
         /// <remarks>
@@ -148,6 +139,15 @@ namespace Software_Reengineering
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink HyperLink6;
+
+        /// <summary>
+        /// HyperLink5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink5;
 
         /// <summary>
         /// ContentPlaceHolder1 control.
